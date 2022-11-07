2f0a1b8c79fdc8679428a39810efcd87d0fbb7cb
hell-llex <demeshenko333@gmail.com>
2022-11-07 03:23:08 +0300

    feat: complete 4-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,45 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+  if (err) throw err;
+});
+fs.readdir(
+  path.join(__dirname, 'files-copy'),
+  { withFileTypes: true },
+  (err, filesData) => {
+    if (err)
+      console.log('К сожалению, в процессе работы произошла ошибка! :(\n', err);
+    else {
+      for (const file of filesData) {
+        fs.unlink(path.join(__dirname, 'files-copy', file.name), (err) => {
+          if (err) throw err;
+        });
+      }
+    }
+  }
+);
+
+fs.readdir(
+  path.join(__dirname, 'files'),
+  { withFileTypes: true },
+  (err, filesData) => {
+    if (err)
+      console.log('К сожалению, в процессе работы произошла ошибка! :(\n', err);
+    else {
+      for (const file of filesData) {
+        fs.copyFile(
+          path.join(__dirname, 'files', file.name),
+          path.join(__dirname, 'files-copy', file.name),
+          (err) => {
+            if (err) throw err;
+          }
+        );
+      }
+    }
+  }
+);
+
+process.on('exit', () => console.log(
+  '\n-----------------------------------------------------------------------\n                 Директория была успешно скопированна :)\n-----------------------------------------------------------------------'
+));
